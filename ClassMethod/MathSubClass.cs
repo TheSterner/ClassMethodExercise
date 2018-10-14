91b0d015b800bbde704a4508b3ff6e4ac15a9b0a
William B McAllister <blakemcallister77@gmail.com>
2018-10-14 14:16:37 -0400

    pg 117

---
+++ ClassMethod/MathSubClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+	public class MathSubClass
+	{
+		public void DivUserNum(int userNum)
+		{
+			//TempUserNum2 = new int(TempUserNum);
+
+			for (int j = 0; j < userNum;)
+			{
+				int divUserNum1 = userNum / 2;
+				//userNum = divUserNum1;
+				Console.WriteLine("Your number divided by 2 is: " + divUserNum1);
+				return;
+			}
+		}
+
+		public int TempUserNum { get; set; }
+
+		public static int TimesUserNumber(int j)
+		{
+			return j * 3;
+		}
+
+
+		//public void SubUserNum(int userNum2, int times =1)
+		//{
+		//	for (int i=0; i< userNum2; i++)
+		//	{
+		//		int subUserNum = userNum2 - 10;
+		//		userNum2 = subUserNum;
+		//	}
+
+		//}
+	}
+}
--- ClassMethod/Program.cs
+++ ClassMethod/Program.cs
@@ -10,6 +10,14 @@ namespace ClassMethod
 	{
 		static void Main(string[] args)
 		{
+			MathSubClass mathSubClass = new MathSubClass();
+			Console.WriteLine("Pick pick a number between 1-50.");
+			int userNumIn = Convert.ToInt32(Console.ReadLine());
+			mathSubClass.DivUserNum(userNumIn);
+			int timesUserNum = MathSubClass.TimesUserNumber(userNumIn);
+			Console.WriteLine("Your number times 3 is: " + timesUserNum);
+			Console.ReadLine();
+
 		}
 	}
 }
